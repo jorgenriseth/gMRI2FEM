2885ed4f6196cb3a06bff5fe8b2f1aa33c132f50
jorgenriseth <jnriseth@gmail.com>
2024-05-21 21:16:38 +0200

    updates to analysis pipeline

--- gmri2fem/mriprocessing/estimate_concentration.py
+++ gmri2fem/mriprocessing/estimate_concentration.py
@@ -19,20 +19,25 @@ if __name__ == "__main__":
     parser.add_argument("--mask", type=Path, default=None)
     args = parser.parse_args()
 
-    reference_volume = nibabel.freesurfer.mghformat.load(args.reference)
-    reference_affine = reference_volume.header.get_vox2ras()
+    if args.input.suffix in [".mgz", ".mgh"]:
+        volume = nibabel.freesurfer.mghformat.load(args.input)
+        reference_volume = nibabel.freesurfer.mghformat.load(args.reference)
+    else:
+        volume = nibabel.nifti1.load(args.input)
+        reference_volume = nibabel.nifti1.load(args.reference)
+
+    reference_affine = reference_volume.affine
     reference_data = reference_volume.get_fdata(dtype=np.float32)
 
-    volume = nibabel.freesurfer.mghformat.load(args.input)
     assert np.allclose(
-        reference_affine, volume.header.get_vox2ras()
+        reference_affine, volume.affine
     ), "Affine transformations differ, are you sure the images are registered properly?"
     data = volume.get_fdata(dtype=np.single)
 
     if args.mask is not None:
         mask = nibabel.freesurfer.mghformat.load(args.mask)
         assert np.allclose(
-            reference_affine, mask.header.get_vox2ras()
+            reference_affine, mask.affine
         ), "Affine transformations differ, are you sure the baseline and T1 Map are registered properly?"
         mask = mask.get_fdata(dtype=np.single).astype(bool)
         reference_data *= mask
@@ -43,7 +48,12 @@ if __name__ == "__main__":
         data[~mask] = np.nan
 
     concentration = concentration_from_T1(T1=data, T1_0=reference_data, r1=args.r1)
-    nibabel.freesurfer.mghformat.save(
-        nibabel.freesurfer.mghformat.MGHImage(concentration, reference_affine),
-        args.output,
-    )
+    if args.output.suffix in [".mgz", ".mgh"]:
+        nibabel.freesurfer.mghformat.save(
+            nibabel.freesurfer.mghformat.MGHImage(concentration, reference_affine),
+            args.output,
+        )
+    else:
+        nibabel.nifti1.save(
+            nibabel.nifti1.Nifti1Image(concentration, reference_affine), args.output
+        )
