17e25ed74dc88ad77fd62596a7eb07cebf831702
Jørgen Riseth <jnriseth@gmail.com>
2025-05-21 13:12:34 +0200

    merged duplicate statistics-functions

--- src/gmri2fem/regionwise_statistics.py
+++ src/gmri2fem/regionwise_statistics.py
@@ -1,82 +1,22 @@
-import json
 from pathlib import Path
 
-import nibabel.nifti1 as nifti1
 import numpy as np
 import pandas as pd
+import tqdm
 from simple_mri import assert_same_space, load_mri
-from tqdm import tqdm
 
 from gmri2fem.segmentation_groups import default_segmentation_groups
+from gmri2fem.segment_tools import read_lut
 
-
-def create_dataframe(
-    subject: str,
-    session: str,
-    sequence: str,
-    timestamp_sequence: str,
-    mri_path: Path,
-    seg_path: Path,
-    lut_path: Path,
-    timestamps_path: Path,
-) -> pd.DataFrame:
-    with open(lut_path, "r") as f:
-        fs_lut = json.load(f)
-
-    seg = nifti1.load(seg_path).get_fdata(dtype=np.single).astype(np.int32)
-    seg_mri = load_mri(seg_path, dtype=np.int16)
-    data_mri = load_mri(mri_path, dtype=np.single)
-    assert_same_space(seg_mri, data_mri)
-    seg, data = seg_mri.data, data_mri.data
-
-    # Extract all freesurfer-lookuptable values and region names present in segmentation
-    seg_labels = np.unique(seg[seg != 0])
-    seg_regions = [fs_lut[str(label)] for label in seg_labels]
-
-    regions = {
-        **{region: [label] for region, label in zip(seg_regions, seg_labels)},
-        **default_segmentation_groups(),
-    }
-
-    timestamp = find_timestamp(timestamps_path, timestamp_sequence, subject, session)
-    regions_stat_functions = {
-        "mean": np.mean,
-        "median": np.median,
-        "std": np.std,
-        "PC1": lambda x: np.quantile(x, 0.01),
-        "PC5": lambda x: np.quantile(x, 0.05),
-        "PC95": lambda x: np.quantile(x, 0.95),
-        "PC99": lambda x: np.quantile(x, 0.99),
-    }
-
-    timestamp = max(0, timestamp)
-    records = []
-    for name, labels in tqdm(regions.items()):
-        regions_data = data[np.isin(seg, labels)]
-        regions_data = regions_data[np.isfinite(regions_data)]  # Keep only reals
-
-        if regions_data.size == 0:
-            continue
-
-        group_regions = {
-            **{
-                "FS_LUT-labels": ",".join([str(x) for x in labels]),
-                "FS_LUT-region": name,
-                "FS_LUT-voxelcount": seg[np.isin(seg, labels)].size,
-                "subject": subject,
-                "session": session,
-                "sequence": sequence,
-                "time": timestamp,
-                "region_total": np.sum(regions_data),
-            },
-            **{
-                f"{stat}": func(regions_data)
-                for stat, func in regions_stat_functions.items()
-            },
-        }
-        records.append(group_regions)
-
-    return pd.DataFrame.from_records(records)
+REGION_STAT_FUNCTIONS = {
+    "mean": np.mean,
+    "median": np.median,
+    "std": np.std,
+    "PC1": lambda x: np.quantile(x, 0.01),
+    "PC5": lambda x: np.quantile(x, 0.05),
+    "PC95": lambda x: np.quantile(x, 0.95),
+    "PC99": lambda x: np.quantile(x, 0.99),
+}
 
 
 def find_timestamp(
@@ -97,6 +37,70 @@ def find_timestamp(
         print(timestamp_sequence, subject, session)
         raise e
     return timestamp.item()
+
+
+def create_dataframe(
+    subject: str,
+    session: str,
+    sequence: str,
+    timestamp_sequence: str,
+    mri_path: Path,
+    seg_path: Path,
+    timestamps_path: Path,
+) -> pd.DataFrame:
+    seg_mri = load_mri(seg_path, dtype=np.int16)
+    data_mri = load_mri(mri_path, dtype=np.single)
+    assert_same_space(seg_mri, data_mri)
+    seg, data = seg_mri.data, data_mri.data
+
+    seg_labels = np.unique(seg[seg != 0])
+    fs_lut = read_lut(None)
+    seg_descriptions = [
+        fs_lut["description"][fs_lut["label"] == label].item() for label in seg_labels
+    ]
+    regions = {
+        **{region: [int(label)] for region, label in zip(seg_descriptions, seg_labels)},
+        **default_segmentation_groups(),
+    }
+    dframe = compute_region_statistics(data, seg, regions)
+
+    timestamp = find_timestamp(timestamps_path, timestamp_sequence, subject, session)
+    timestamp = max(0, timestamp)
+
+    new_cols = ["subject", "session", "sequence", "timestamp"] + list(dframe.columns)
+    newframe = dframe.assign(
+        subject=subject, session=session, sequence=sequence, timestamp=timestamp
+    )
+    return newframe.loc[:, new_cols]
+
+
+def compute_region_statistics(
+    volume: np.ndarray, seg_vol: np.ndarray, regions: dict[str, list[int]]
+) -> pd.DataFrame:
+    records = []
+    finite_mask = np.isfinite(volume)
+    for description, labels in tqdm.tqdm(regions.items()):
+        region_mask = np.isin(seg_vol, labels) * finite_mask
+        region_data = volume[region_mask]
+
+        voxelcount = region_mask.sum()
+        if voxelcount == 0:
+            continue
+
+        group_regions = {
+            **{
+                "FS_LUT-labels": ",".join([str(x) for x in labels]),
+                "FS_LUT-region": description,
+                "FS_LUT-voxelcount": voxelcount,
+                "region_total": np.sum(region_data),
+            },
+            **{
+                f"{stat}": func(region_data) if voxelcount > 0 else np.nan
+                for stat, func in REGION_STAT_FUNCTIONS.items()
+            },
+        }
+        records.append(group_regions)
+    return pd.DataFrame.from_records(records)
 
 
 if __name__ == "__main__":
@@ -123,7 +127,6 @@ if __name__ == "__main__":
         timestamp_sequence=args.timestamp_sequence,
         mri_path=args.data,
         seg_path=args.seg,
-        lut_path=args.lutfile,
         timestamps_path=args.timestamps,
     )
     dframe.to_csv(Path(args.output), index=False, sep=";")
