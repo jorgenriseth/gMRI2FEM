07742e594ef3230a18acb7b6e6b3f1af4bf9fb33
Jørgen Riseth <jnriseth@gmail.com>
2025-08-02 17:02:08 +0200

    added boundary refinement CLI command

--- src/brainmeshing/boundary_cell_refinement.py
+++ src/brainmeshing/boundary_cell_refinement.py
@@ -2,47 +2,71 @@ import click
 import dolfin as df
 import numpy as np
 import pantarei as pr
+import tqdm
+
+from glymphopt.utils import with_suffix
+
+df.parameters["refinement_algorithm"] = "plaza_with_parent_facets"
 
 
 @click.command("refine_mesh")
 @click.option("--input", "-i", type=str, required=True)
 @click.option("--output", "-o", type=str, required=True)
 @click.option("--threshold", "-t", type=float, default=2.0)
-@click.option("--function", "-f", "functions", type=str, multiple=True)
-def refine_mesh_cli(input, output, threshold, functions):
+@click.option("--maxiter", type=int, default=5)
+def refine_mesh_cli(input, output, threshold, maxiter):
     with df.HDF5File(df.MPI.comm_world, input, "r") as hdf:
         domain = pr.read_domain(hdf)
-    refined_mesh = refine_mesh(domain, threshold)
+    refined_domain = refine_mesh(domain, threshold, max_iter=maxiter)
 
-    # TODO: Fix meshfunction reader.
-    # TODO: Add facet-function mapping.
-    for function in functions:
-        subdomains = pr.read_function()
-        map_mesh_function(subdomains, domain, refined_mesh)
+    with df.HDF5File(df.MPI.comm_world, output, "w") as hdf:
+        pr.write_domain(hdf, refined_domain)
 
-    # TODO: Add writer to both HDF and XDMF.
+    with df.XDMFFile(
+        df.MPI.comm_world, str(with_suffix(output, "_subdomains.xdmf"))
+    ) as xdmf:
+        xdmf.write(refined_domain.subdomains)
+
+    with df.XDMFFile(
+        df.MPI.comm_world, str(with_suffix(output, "_boundaries.xdmf"))
+    ) as xdmf:
+        xdmf.write(refined_domain.boundaries)
 
 
 def is_boundary_cell(cell: df.Cell) -> bool:
     return any(facet.exterior() for facet in df.facets(cell))
 
 
-def refine_mesh(mesh, threshold):
-    init_mesh = df.Mesh(mesh)
+def refine_mesh(domain, threshold, max_iter=5):
+    init_mesh = df.Mesh(domain)
     potential_boundary_cells = df.MeshFunction(
         "size_t", init_mesh, init_mesh.topology().dim(), 1
     )
+    init_subdomains = df.MeshFunction(
+        "size_t", init_mesh, init_mesh.topology().dim(), 0
+    )
+    init_subdomains.array()[:] = domain.subdomains.array()
+    init_boundaries = df.MeshFunction(
+        "size_t", init_mesh, init_mesh.topology().dim() - 1, 0
+    )
+    init_boundaries.array()[:] = domain.boundaries.array()
 
-    while True:
+    iter = 0
+    while iter < max_iter:
+        iter += 1
         init_mesh.init()
         cells_marked_for_refinement = df.MeshFunction(
             "bool", init_mesh, init_mesh.topology().dim(), False
         )
-
         N = init_mesh.num_cells()
         is_large_boundary_cell = np.zeros(N, dtype=bool)
         num_cells_marked_for_refinement = 0
-        for cell in filter(lambda c: potential_boundary_cells[c], df.cells(init_mesh)):  # type: ignore
+        n = potential_boundary_cells.array()[:].sum()
+        cell_generator = filter(
+            lambda c: potential_boundary_cells[c],  # type: ignore
+            df.cells(init_mesh),
+        )
+        for cell in tqdm.tqdm(cell_generator, total=n):
             if is_boundary_cell(cell) and cell.circumradius() > threshold:
                 is_large_boundary_cell[cell.index()] = True
 
@@ -55,8 +79,16 @@ def refine_mesh(mesh, threshold):
         refined_mesh = df.refine(init_mesh, cells_marked_for_refinement)
         potential_boundary_cells = df.adapt(potential_boundary_cells, refined_mesh)
         init_mesh = refined_mesh
+        init_subdomains = df.adapt(init_subdomains, refined_mesh)
+        init_boundaries = df.adapt(init_boundaries, refined_mesh)
 
-    return init_mesh
+    newfacets = init_boundaries.array() > len(init_boundaries.array())
+    init_boundaries.array()[np.argwhere(newfacets)] = 0
+    return pr.Domain(
+        init_mesh,
+        init_subdomains,
+        init_boundaries,
+    )
 
 
 def map_mesh_function(source_mesh_func, source_mesh, dest_mesh):
